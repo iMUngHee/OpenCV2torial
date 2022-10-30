17e991252a0b7fa66c68c85ec967db3d73a7f640
LUH <unghee66@gmail.com>
2022-10-30 14:39:58 +0900

    [Session] 12. Histogram Computation

---
+++ OpenCV/02/histogram.py
@@ -0,0 +1,44 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = cv.imread('../Resources/Photos/cats.jpg')
+cv.imshow('Origin', img)
+
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray', gray)
+
+mask = cv.circle(blank, (img.shape[1] // 2, img.shape[0] // 2), 100, 255, -1)
+# masked = cv.bitwise_and(img, img, mask=mask)
+masked = cv.bitwise_and(gray, gray, mask=mask)
+cv.imshow('Mask', masked)
+
+# Grayscale Histogram
+gray_hist = cv.calcHist([gray], [0], mask, [256], [0, 256])
+
+plt.figure()
+plt.title('GrayScale Histogram')
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0, 256])
+plt.show()
+
+# Color Histogram
+# plt.figure()
+# plt.title('Color Histogram')
+# plt.xlabel('Bins')
+# plt.ylabel('# of pixels')
+# colors = ('b', 'g', 'r')
+
+# for i, col in enumerate(colors):
+#     hist = cv.calcHist([img], [i], mask, [256], [0, 256])
+#     plt.plot(hist, color=col)
+#     plt.xlim([0, 256])
+
+# plt.show()
+
+
+cv.waitKey(0)
