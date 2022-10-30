6432f51732836f965b4f8bbb271256d2a86a041b
LUH <unghee66@gmail.com>
2022-10-30 11:21:40 +0900

    [Session] 02. Resizing and Rescaling Frames

---
+++ OpenCV/01/rescale.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+
+
+def rescaleFrame(frame, scale=0.75):
+    # Images, Videos and Live Video
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+def changeRes(width, height):
+    # Only for Live Video
+    capture.set(3, width)
+    capture.set(4, height)
+
+
+img = cv.imread('../Resources/Photos/cat.jpg')
+resized_image = rescaleFrame(img, 3)
+print(resized_image.shape)
+cv.imshow('Image', img)
+cv.imshow('Image', resized_image)
+
+cv.waitKey(0)
+
+# Reading Videos
+capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame, .2)
+
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
