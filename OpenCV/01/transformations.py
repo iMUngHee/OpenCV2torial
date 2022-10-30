db2ad0c020759bbb9f1b5946e9ee7fc15d1d4325
LUH <unghee66@gmail.com>
2022-10-30 12:38:00 +0900

    [Session ] 05. Image Transformations

---
+++ OpenCV/01/transformations.py
@@ -0,0 +1,63 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('../Resources/Photos/park.jpg')
+
+cv.imshow('Origin', img)
+
+# ! Translation
+
+
+def translate(img, x, y):
+    trans_mat = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+
+    return cv.warpAffine(img, trans_mat, dimensions)
+
+
+"""
+    * -x --> Left
+    * -y --> Up
+    * x --> Right
+    * y --> Down
+"""
+translated = translate(img, 100, 100)
+cv.imshow('Translated', translated)
+
+# ! Rotation
+
+
+def rotate(img, angle, rotPoint=None):
+    (height, width) = img.shape[:2]
+
+    if rotPoint is None:
+        rotPoint = (width // 2, height // 2)
+
+    rot_mat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, rot_mat, dimensions)
+
+
+rotated = rotate(img, 45)
+cv.imshow('Rotated', rotated)
+
+# ? Not Working
+rotated_rotated = rotate(rotated, 45)
+cv.imshow('Rotated', rotated_rotated)
+
+# Resizing
+# Interpolation(보간법): https://deep-learning-study.tistory.com/185
+resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+# Flipping
+# 0: Ver,  1: Hor, -1: Both
+flipped = cv.flip(img, 0)
+cv.imshow('Flip', flipped)
+
+# Cropping
+cropped = img[200:400, 300:400]
+cv.imshow('Cropped', cropped)
+
+cv.waitKey(0)
